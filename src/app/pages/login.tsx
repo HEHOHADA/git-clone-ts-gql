610f387236149e116cf5eddd5ad325747353ddfb
Aida <aidagalimova03@gmail.com>
2020-10-27 15:02:32 +0300

    fixes...)

--- src/app/pages/login.tsx
+++ src/app/pages/login.tsx
@@ -1,21 +1,10 @@
 import React from "react";
 import Layout from "../components/layout/MainLayout";
-import { makeStyles } from "@material-ui/core";
-import Log from "../components/Login/Login";
+import LoginForm from "../components/Login/Login";
 export default function Login() {
-  const classes = useStyles();
   return (
-    <div className={classes.container}>
-      <Layout>
-        <Log />
-      </Layout>
-    </div>
+    <Layout>
+      <LoginForm />
+    </Layout>
   );
 }
-
-const useStyles = makeStyles(() => ({
-  container: {
-    height: "100vh",
-    backgroundColor: "#2b3137",
-  },
-}));
