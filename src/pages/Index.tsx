7b6adf997f69f5712de40d1d866731e73730ca9f
Ural <ural2001@gmail.com>
2021-01-06 19:21:25 +0300

    Make contributions section

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -14,29 +14,43 @@ export default function Index() {
   const classes = useStyles()
   const {data, loading, error} = useGetViewerReposQuery()
   const repositories = data?.viewer.repositories.edges
+  const contributions = data?.viewer.contributionsCollection.commitContributionsByRepository
   const [search, setSearch] = useState<string>('')
   const searchInputChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value)
   }, [])
   console.log('data', data)
+  console.log('contributions', contributions)
   return (
     <MainLayout>
       <MainContainer>
-        <SideBarContainer>
-          <div className={ classes.centeredContainer }>
-            <span>Repositories</span>
-            <IconButton color="inherit">
-              <LibraryAdd/>
-            </IconButton>
-          </div>
-          <Search placeholder={ 'Поиск репозитория' } onChange={ searchInputChange }/>
-          <DataShower error={ error }
-                      loading={ loading }
-                      data={ repositories }
-                      DataComponent={ <Repositories
-                        search={ search }
-                        repositories={ repositories as Array<RepositoryEdge> }/> }/>
-        </SideBarContainer>
+        <>
+            <SideBarContainer>
+            <div className={ classes.centeredContainer }>
+                <span>Repositories</span>
+                <IconButton color="inherit">
+                <LibraryAdd/>
+                </IconButton>
+            </div>
+            <Search placeholder={ 'Поиск репозитория' } onChange={ searchInputChange }/>
+            <DataShower error={ error }
+                        loading={ loading }
+                        data={ repositories }
+                        DataComponent={ <Repositories
+                            search={ search }
+                            repositories={ repositories as Array<RepositoryEdge> }/> }/>
+            </SideBarContainer>
+            <div className={ classes.contributionsSection }>
+                <h1 className={ classes.contribCount }>contributions - number</h1>
+                <div className={ classes.contributions }>
+                    <div className={classes.contribution}>
+                        <h3 className={classes.contributionName}>Repo name</h3>
+                        <span className={classes.contributionName}>contrib name</span>
+                        <span>contrib date</span>
+                    </div>
+                </div>
+            </div>
+        </>
       </MainContainer>
     </MainLayout>
   )
@@ -47,5 +61,29 @@ const useStyles = makeStyles(() => ({
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'space-around',
+  },
+  contributionsSection: {
+      width: '100%',
+  },
+  contribCount: {
+      textAlign: 'center',
+  },
+  contributions: {
+      display: 'grid',
+      gridTemplateColumns: '1fr 1fr',
+      gridGap: '10px',
+      padding: '30px',
+  },
+  contribution: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      padding: '15px 10px',
+      boxShadow: '0 5px 10px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)',
+      borderRadius: '10px',
+
+  },
+  contributionName: {
+      marginRight: '10px',
   }
 }))
