e9fc24b6bae42b5da0d823d52b3ee778256328d7
Ural <ural2001@gmail.com>
2021-01-06 20:07:14 +0300

    Make repository page

--- src/pages/RepositoryPage.tsx
+++ src/pages/RepositoryPage.tsx
@@ -3,6 +3,8 @@ import { MainLayout } from '../components/layout/MainLayout'
 import { useParams } from 'react-router-dom'
 import { useGetRepoQuery } from '../generated/graphql'
 import { MainContainer } from '../components/ui/MainContainer'
+import { makeStyles } from '@material-ui/core/styles'
+import fileIcon from '../static/blank-file.png'
 
 type ParamsType = {
   repo: string
@@ -10,6 +12,7 @@ type ParamsType = {
 }
 
 export default function RepositoryPage() {
+  const classes = useStyles()
   const params = useParams<ParamsType>()
   const {data} = useGetRepoQuery({
     variables: {
@@ -21,8 +24,71 @@ export default function RepositoryPage() {
   return (
     <MainLayout>
       <MainContainer>
-        <div>str</div>
+        <div>
+      <div className={ classes.description }>
+        <h1>Repo name</h1>
+        <h3>Repo owner: </h3>
+        <h3>Contributors: </h3>
+      </div>
+
+            <hr/>
+            <table>
+                <tr>
+                    <td><img src={ fileIcon } className={ classes.icon } alt="File Icon"/></td>
+                    <td>File name</td>
+                </tr>
+            </table>
+            <hr/>
+
+            <div className={ classes.contributionsSection }>
+                <div className={ classes.contributions }>
+                    <div className={classes.contribution}>
+                        <h3 className={classes.contributionName}>contrib name</h3>
+                        <span className={classes.contributionName}>who</span>
+                        <span>contrib date</span>
+                    </div>
+                </div>
+            </div>
+        </div>
       </MainContainer>
     </MainLayout>
   )
 }
+
+const useStyles = makeStyles(() => ({
+centeredContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+},
+description: {
+  marginBottom: '30px'
+},
+contributionsSection: {
+    width: '100%',
+},
+contribCount: {
+    textAlign: 'center',
+},
+contributions: {
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr',
+    gridGap: '10px',
+    padding: '30px 0',
+},
+contribution: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '15px 10px',
+    boxShadow: '0 5px 10px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)',
+    borderRadius: '10px',
+},
+contributionName: {
+    marginRight: '10px',
+},
+icon: {
+    width: '20px',
+  },
+}))
+
